628f7ac74e13132c11ab4f17101877b43fe53803
Axelwickm <axelwickm@gmail.com>
2019-02-13 22:43:12 +0100

    Post and Corpus classes for clarity, and for knowing what data belongs together.

--- language_analyser.py
+++ language_analyser.py
@@ -3,75 +3,104 @@
 import reddit_crawler
 
 import argparse
+import uuid
 
 
-def create_corpus(subreddit):
-    """
-    Collects and generates a tokenized corpus for the given subreddit
-    using the reddit_crawler
-    """
+class Post:
+    """ Representing data held in posts"""
 
-    # FIXME: this is just a temporary structure for the corpus.
-    # It will also have to include time/date, link, user?, unique post id?
-    return [["i", "really", "hate", "popcorn."],
-            ["popcorn", "is", "the", "best", "thing",
-             "since", "sliced", "bread"]]
+    def __init__(self, tokens, time, user, is_comment, url):
+        """Initiate with data about the post obtained from crawler"""
+        self.id = uuid.uuid4().hex
+        self.tokens = tokens
+        self.time = time
+        self.user = user
+        self.is_comment = is_comment
+        self.url = url
+        # TODO: add language?
 
+        # Feature representation of the post
+        self.analyzed_data = {}
 
-def analyze_data(corpus):
-    """
-    Perform various analyses on the data.
-    Return a highdimentional vector representation for each post
-    """
+        # Analyzed data with reduced dimensionality
+        self.reduced_data = ()
 
-    return [[0.5, 0.1, 0.3, 0.8, 0.3], [0.0, 0.8, 0.4, 0.2, 0.35]]
+class Corpus:
+    def __init__(self, subreddit):
+        self.subreddit = subreddit
+        self.posts = []  # This is a list so we can sort it later
 
+    def add_post(self, post):
+        self.posts.append(post)
 
-def cluster_data(representation):
-    """
-    Uses a clustering algorithm to convert the high dimensional into
-    2D points for every post.
-    Returns a list of these 2D points.
-    """
+    def build(self):
+        """
+        Collects and generates a tokenized corpus for the given subreddit
+        using the reddit_crawler
+        """
 
-    return [[0.1, 0.9], [0.5, 0.2]]
+        # FIXME: this is just a temporary structure for the corpus.
+        post_a = Post(["today", "i", "learned", "you", "eat", "popcorn", "microwaved"], 13572134687, "axelwickm", False,
+                      "https://www.reddit.com/r/CasualConversation/comments/95hpj2/today_i_learned_that_you_eat_popcorn_microwaved/")
+        self.add_post(post_a)
+
+        post_b = Post(["popcorn", "is", "the", "best", "thing", "since", "sliced", "bread"], 135721842345,
+                      "thisisbillgates", True,
+                      "https://www.reddit.com/r/CasualConversation/comments/95hpj2/today_i_learned_that_you_eat_popcorn_microwaved/comments=53774")
+        self.add_post(post_b)
+
+    def sort_posts(self):
+        """ TODO:sort posts with oldest first """
+        pass
+
+    def perform_analysis(self):
+        """
+        Perform various analyses on the data.
+        Save a high dimensional representation each post
+        """
+
+        self.posts[0].analyzed_data = {"sentiment": [0.5, 0.1, 0.3], "length": 30, "complexity": 0.92}
+        self.posts[1].analyzed_data = {"sentiment": [0.8, 0.2, 0.8], "length": 10, "complexity": 0.12}
+
+    def reduce_data_dimensions(self):
+        """
+        Uses a dimensionality reduction algorithm to convert the high dimensional into
+        2D points for every post.
+        Returns a list of these 2D points.
+        """
+
+        self.posts[0].reduced_data = (0.3, 0.7)
+        self.posts[1].reduced_data = (0.8, 0.5)
 
 
 def main(subreddits):
     """ Generate, analyze, cluster and visualize data from given subreddits """
 
-    # The data is collected from reddit and corpuses are stored in a dictionary
     corpuses = {}
     for subreddit in subreddits:
+        # The data is collected from reddit and corpuses are stored in a dictionary
         print("Creating corpus: /r/"+subreddit)
-        corpuses[subreddit] = create_corpus(subreddit)
+        corpus = Corpus(subreddit)
+        corpus.build()
 
-    print("")
+        # Features are extracted from the corpuses and stored in as high
+        # dimensional representations
+        print("Analyzing corpus: /r/" + subreddit)
+        corpus.perform_analysis()
 
-    # Features are extraced from the corpuses and stored in as high
-    # dimensional representations
-    representations = {}
-    for subreddit, corpus in corpuses.items():
-        print("Analyzing corpus; /r/"+subreddit)
-        representations[subreddit] = analyze_data(corpus)
+        # This data is then converted into 2D points
+        print("Reducing corpus representation: /r/" + subreddit)
+        corpus.reduce_data_dimensions()
 
-    print("")
-
-    # This data is then converted into 2D points, and saved to a file with
-    # other meta information
-    for subreddit, representation in representations.items():
-        # Run the clustering algorthim
-        print("Clustering corpus representation: /r/"+subreddit)
-        clustered = cluster_data(representation)
-
-        # Save to file immediately to /data/ directory
-        # We don't need it in the rest of the program
-        print("Saving clustered corpus: /r/"+subreddit+"\n")
+        # Save to dictionary and to file in /data/ directory
+        corpuses[subreddit] = corpus
+        print("Saving clustered corpus: /r/" + subreddit + "\n\n")
         # TODO: save to file
 
     # TODO: open HTTP server
 
     # TODO: tell default browser to open web server address and visualize data
+
 
 if __name__ == "__main__":
     # Figure out what algoritms the user wants to analyze.
