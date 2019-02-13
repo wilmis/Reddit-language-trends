2484ace9e88118d2e22c70b5878f3d90ec7cef4b
Axel Wickman <axelwickm@gmail.com>
2019-02-13 11:39:28 +0100

    Argument parsing. Can enter what subreddits can analyze when starting the program.

--- language_analyser.py
+++ language_analyser.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
 import reddit_crawler
+
+import argparse
+
+def main(subreddits):
+    print(subreddits)
+
+if __name__ == "__main__":
+    # Figure out what algoritms the user wants to analyze. 
+    # This is fed into the program by typing for example:
+    # python language_analyzer.py aww me_irl
+    parser = argparse.ArgumentParser(description="What to analyse")
+    parser.add_argument('subreddit', metavar="s", type=str, nargs='*',
+        help='What subreddits to analyze')
+    args = parser.parse_args()
+    subreddits = args.subreddit
+    # If no subreddits 
+    if len(subreddits) == 0:
+        subreddits.append("sweden")
+
+    main(subreddits)
