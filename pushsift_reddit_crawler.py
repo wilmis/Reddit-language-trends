fe806a2b1fd06dda401fb81116be9d9dc7efd80c
wilmis <47592651+wilmis@users.noreply.github.com>
2019-02-22 14:58:13 +0100

    Update pushsift_reddit_crawler.py

---
+++ pushsift_reddit_crawler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+# skapa funktion för att bestämma vilket tidsspann som ska sökas över
+
+# plocka hem id på de submissions som förkommer mellan det tidsspannet
+
+# skicka de till reddit_crawler för att plockain data.
